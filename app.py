90a19a36c3b8c331f66cd291be91766eac2c9521
JamalGuyo <jamalguyo08@gmail.com>
2022-10-28 11:39:54 +0300

    connect app to db, create model

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 #============ FLASK CONFIGURATION ==========================#
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://jamal:0987jJ@localhost:5432/todoapp'
+app.app_context().push()
+db = SQLAlchemy(app)
 
+#============ DATABASE MODELS ==========================#
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo id={self.id} description={self.description}>'
+
+db.create_all()
 
 #=========== ROUTES =========================================#
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {'description': 'Todo1'},
-        {'description': 'Todo2'},
-        {'description': 'Todo3'},
-        {'description': 'Todo4'}
-    ])
+    return render_template('index.html', data=Todo.query.all())
